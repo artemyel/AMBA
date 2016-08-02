ccdbe0b4bb5a5f953078b1c6ca62cfcc2a0ee4ae
alexnar <sasha_nar@mail.ru>
2016-08-03 00:55:12 +0300

    base template + migration Profile BD

--- AMBA/settings.py
+++ AMBA/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'AMBA.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR,'templates').replace('\\','/')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -124,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import Category
 
 # Create your views here.
+class IndexView(generic.ListView):
+    model = Category
+    template_name = 'main/index.html'
