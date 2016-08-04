0d70eb957b8844c46e6ffcbb45b6fa09c36b391a
artemyel <artemyel@gmail.com>
2016-08-04 22:39:45 +0300

    login fix + logout

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'main'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^logout/$', views.logout_view, name='logout_view')
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render, redirect
 from .forms import LogginForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 
 def index(request):
     if request.method == 'POST':
         form = LogginForm(request.POST)
         if form.is_valid():
-            user = authenticate(username=form.username, password=form.password)
+            user = authenticate(email=form.cleaned_data['username'], password=form.cleaned_data['password'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
                     return render(request, 'main/index.html', {'user': user})
     else:
-        form = LogginForm()
-        return render(request, 'main/index.html', {'form': form})
+        if request.user.is_authenticated:
+            return render(request, 'main/index.html', {'user': request.user})
+        else:
+            form = LogginForm()
+            return render(request, 'main/index.html', {'form': form})
 
+
+def logout_view(request):
+    logout(request)
+    return redirect('/main/')
 
