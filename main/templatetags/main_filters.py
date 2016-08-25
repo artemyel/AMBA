dcd4154b0936526e8ba5044c0ac3eb5c6d39980d
artemyel <artemyel@gmail.com>
2016-08-25 18:33:16 +0300

    offers url fix

--- main/templatetags/main_filters.py
+++ main/templatetags/main_filters.py
@@ -7,3 +7,4 @@ register = template.Library()
 @register.filter(name='get_image_list_by_offer')
 def get_image_list_by_offer(offerID):
     return OfferImage.objects.all().filter(offer=offerID)
+
--- main/views.py
+++ main/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import LogginForm, QueryForm
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from main.models import Category, Offer, OfferImage
+from main.models import Category, Offer, OfferImage, CommunityProduct
 from django.views.generic import ListView
 
 
@@ -15,14 +15,14 @@ def index(request):
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return render(request, 'main/index.html', {'user': user})
+                    return render(request, 'main/index.html', {'user': user, 'categories': Category.objects.filter()})
     # TODO отделить log_in view от main view
     else:
         if request.user.is_authenticated:
-            return render(request, 'main/index.html', {'user': request.user})
+            return render(request, 'main/index.html', {'user': request.user, 'categories': Category.objects.filter()})
         else:
             form = LogginForm()
-            return render(request, 'main/index.html', {'user_form': form})
+            return render(request, 'main/index.html', {'user_form': form, 'categories': Category.objects.filter()})
 
 
 class CategoryView(ListView):
@@ -34,10 +34,18 @@ class CategoryView(ListView):
 
     def get_queryset(self):
         self.form = QueryForm(self.request.GET)
+        category = self.kwargs['category_name']
+        qs = []
+        products = CommunityProduct.objects.filter(category=Category.objects.filter(short_name=category))
         if self.form.is_valid():
-            qs = Offer.objects.filter(price__lt=self.form.cleaned_data['price']).order_by()
+            #qs = Offer.objects.filter(product=product, price__lt=self.form.cleaned_data['price']).order_by()
+            for product in products:
+                if Offer.objects.filter(product=product, price__lt=self.form.cleaned_data['price']):
+                    qs.append(Offer.objects.get(product=product, price__lt=self.form.cleaned_data['price']))
         else:
-            qs = Offer.objects.filter().order_by()
+            #qs = Offer.objects.filter(product=product).order_by()
+            for product in products:
+                qs.append(Offer.objects.get(product=product))
         if not self.request.user.is_authenticated():
             return qs.exclude()
         return qs
@@ -47,6 +55,13 @@ class CategoryView(ListView):
         context['form'] = self.form
         context['image_list'] = OfferImage.objects.all()
         return context
+
+    def get(self, request, *args, **kwargs):
+        category = self.kwargs['category_name']
+        if not Category.objects.filter(short_name=category):
+            return redirect('/main/')
+        else:
+            return super(CategoryView, self).get(request, *args, **kwargs)
 
 
 def offer_view(request, offer_id):
