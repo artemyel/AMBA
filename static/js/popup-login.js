433e2a174317d7f6574eb06b6afb74d846f9da43
koshe4ka <smiledron@mail.ru>
2016-09-06 19:10:52 +0300

    fix avatar, new slideToggle

--- static/js/popup-login.js
+++ static/js/popup-login.js
@@ -1,9 +1,34 @@
-function PopUpSho() {
-    $("#popup1").show()
-}
+
 
 $(document).ready(function(){
     $('.log').click(function(){
         $('.LogBody').slideToggle('fast');
     });
 });
+
+
+
+$(document).ready(function(){
+    $('.user').click(function(){
+        $('.userwindow').slideToggle('fast');
+    });
+});
+
+
+
+function autosize (img,max_width,max_height)
+{
+ if (img.width>max_width)
+ {
+  width = img.width; height = img.height;
+  img.width = max_width;
+  img.height = (max_width*height)/width;
+ }
+
+ if (img.height>max_height)
+ {
+  width = img.width; height = img.height;
+  img.height = max_height;
+  img.width = (max_height*width)/height;
+ }
+}
